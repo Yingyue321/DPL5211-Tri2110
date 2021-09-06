547a5498da7a1b84695dbb0c4463b28465bb685a
lalunez <90165949+lalunez@users.noreply.github.com>
2021-09-06 10:38:13 +0800

    Add files via upload

---
+++ Lab1.1.py
@@ -0,0 +1,7 @@
+# Student ID: 1201201400 #
+# Student Name : Tan Ying Yue #
+
+print("I like Python")
+
+person = input ("Please enter your name: ")
+print("Hello ", person)
---
+++ Lab1.2.py
@@ -0,0 +1,16 @@
+# Lab 1.2 get input two numbers and display the sum of the two numbers
+
+Number1 = int (input("Enter the first number: "))
+Number2 = int (input("Enter the second number: "))
+
+sum = Number1 + Number2
+
+#print("Sum of the two numbers: ",sum)
+
+#Display --> The sum of 5 and 6 is 11
+
+#Mine
+print("The sum of",Number1,"and",Number2,"is",sum)
+
+#Sir
+print("The sum of {} and {} is {}".format(Number1,Number2,sum))
